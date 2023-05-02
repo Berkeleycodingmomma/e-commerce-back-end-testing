c2d0cce9e900e48d2e8fbc3785ed30350ed5dde4
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-02 16:20:31 -0600

    Add Id and category_name columns to category.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
+
+// Below I am requireing the dotenv module to load environment variables and the Sequelize module
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// Create a new instance of Sequelize and connect to the database using the 
+// JAWSDB_URL environment variable if available, otherwise use the DB_NAME, DB_USER, and DB_PASSWORD environment variables to connect to a local MySQL database
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -12,4 +16,5 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
+// Export the sequelize instance for use in other modules
 module.exports = sequelize;
--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
+Category.init({
+  // defing the id columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+  //defining the  catagory_name column
+  catagory_name: {
+    type: DataTypes.STRING,
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'category',
+});
 
 module.exports = Category;

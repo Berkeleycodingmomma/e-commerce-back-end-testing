275a4186b72c61652f669dc53a01c5fbc5d76dc3
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-03 16:23:00 -0600

    added the foerign keys to index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+//Below is the foreign key in the product model
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', 
+});
 
 // Categories have many Products
+//Below is the foreign key in the intermediate model
+Product.belongsToMany(Tag, {
+  through: ProductTag, 
+  foreignKey: 'product_id', 
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+// Below is the foreign key in the Tag model
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  foreignKey: 'tag_id', 
+});
 // Tags belongToMany Products (through ProductTag)
+//Below is the foreign key in the Product model
+Category.hasMany(Product, {
+  foreignKey: 'category_id', 
+});
 
 module.exports = {
   Product,

bb82183220e5f456ca14950290d8362c95dbfda3
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-03 16:33:44 -0600

    checked the file paths in priduct.js

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,21 @@
-// import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
-const sequelize = require('../config/connection');
+// Below we are importing important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Initialize Product model (table)
 class Product extends Model {}
 
-// set up fields and rules for Product model
-Product.init(
-  {
-    id: {
-      type: DataTypes.INTEGER, 
-      allowNull: false, 
-      primaryKey: true, 
-      autoIncrement: true, 
-    },
-    product_name: {
-      type: DataTypes.STRING, 
-      allowNull: false, 
+//Below I am defining the columns for the product table
+Product.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
   price: {
     type: DataTypes.DECIMAL,
@@ -25,17 +23,28 @@ Product.init(
     validate: {
       isDecimal: true,
     }
-  }
-  
-}
-  
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    },
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "category",
+    },
+  },
+}, {
+
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product',
+});
 
 module.exports = Product;

b8188e62b3a2ce6b195a813bf22f6223750b5813
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-03 18:05:04 -0600

    added first two funtions in tag-routes to get/return data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,17 +1,37 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+//Below we are asking to get all tags
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json({ message: "Tags not found!" });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+//Below this functions gets a tag by Id
+router.get("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json({ message: "Tag not found!" });
+  }
 });
+
+
+
 
 router.post('/', (req, res) => {
   // create a new tag

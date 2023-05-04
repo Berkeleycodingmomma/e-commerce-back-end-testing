1431c91ec5410a2254fc6fadcfa1fdb6fc751ed8
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-03 18:13:17 -0600

    added the create, update, and delete functions in tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,19 +30,40 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-
-
-
-router.post('/', (req, res) => {
-  // create a new tag
+//Below we are asking fot a new tag to be create
+router.post("/", async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json({ message: "Tag creation failed" });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// Here we are asking to update a tag by ID
+router.put("/:id", async (req, res) => {
+  try {
+    const updated = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    !updated[0]
+      ? res.status(404).json({ message: "No tag found with this id!" })
+      : res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({ message: "Tag update failed" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+//Now we are aking to delete a tag by ID
+router.delete("/:id", async (req, res) => {
+  try {
+    const deleted = await Tag.destroy({ where: { id: req.params.id } });
+    !deleted
+      ? res.status(404).json({ message: "No tag found with this id!" })
+      : res.status(200).json(deleted);
+  } catch (err) {
+    res.status(500).json({ message: "Tag deletion failed" });
+  }
 });
 
 module.exports = router;
